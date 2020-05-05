18324a0ddcfdf45cf137ec7900aa147061b38b0b
Renato Viana <renatoviana30@gmail.com>
2020-05-04 22:08:37 -0300

    Exibindo dados dinamicamente com Javascript

---
+++ js/cliente.js
@@ -0,0 +1,29 @@
+const informacoesClientes = [
+  {
+    cpf: 12345678545,
+    nome: "Ana",
+  },
+  {
+    cpf: 98765345671,
+    nome: "Julia",
+  },
+];
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+const exibeCliente = (cpf, nome) => {
+  const linha = document.createElement('tr');
+
+  const conteudoLinha = `
+    <td>${cpf}</td>
+    <td>${nome}</td>
+ `;
+
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+};
+
+informacoesClientes.forEach((indice) => {
+  corpoTabela.appendChild(exibeCliente(indice.cpf, indice.cpf));
+});
